9ef296891c2ccd4a11095e4f35241bcd647e6014
Hamza-Sama <hamxu69@gmail.com>
2026-05-06 22:15:32 +0500

    Update making.py

--- making.py
+++ making.py
@@ -1,38 +1,6 @@
-# 6. Ask the user for their name, then print Hello, [name]! Welcome.
-# 7. Ask for two numbers and print their sum, difference, product, and division.
-# 8. Ask for a temperature in Celsius, convert it to Fahrenheit, print the result.
-
-# Formula: F = (C × 9/5) + 32
-
-# 9. Ask for someone's birth year, print how old they are.
-# 10. Ask for a price and a discount percentage, print the final price after discount.
-# 6
-userName = input("State your name here:")
-print(f"Hello, {userName}! Welcome.")
-# 7
-a = int(input("Enter a number:"))
-b = int(input("Enter a again number:"))
-sum = a + b
-product = a * b
-difference = a - b
-division = a / b
-print(f"sum of following numbers is {sum}")
-print(f"product of following numbers is {product}")
-print(f"difference of following numbers is {difference}")
-print(f"division of following numbers is {division}")
-# 8
-TemperatureInCelsius = int(input('Enter Temperature:'))
-InFahrenheit = (TemperatureInCelsius * 9/5) + 32
-print(f"Temperature in Fahrenheit is {InFahrenheit}'F.")
-# 9
-userAge = int(input("Enter your birth year: "))
-current_year = 2026
-outputAge = current_year - userAge
-print(f"You're {outputAge}!")
-# 10
-ogPrice = int(input("Price of watch: "))
-discountPer = int(input("Discount Percentage: "))
-discountPrice = (discountPer/100) * ogPrice
-finalPrice = ogPrice - discountPrice 
-print(finalPrice)
-
+fingers = [0, 1, 1, 0, 0, 0, 1, 1]
+count = 0
+for finger in fingers :
+    if finger == True:
+        count += 1
+print(f"I am pointing {count} fingers")
