53ea612c665cd3a37bf3947b62a64c4ac9848178
Hamza-Sama <hamxu69@gmail.com>
2026-05-11 13:51:33 +0500

    Update gesture_recognition.py

--- gesture_recognition.py
+++ gesture_recognition.py
@@ -1,29 +1,12 @@
-balance = 100000
+first_name = input("Enter Your First Name: ").strip()
+last_name = input("Enter Your Last Name: ").strip()
 
-while True:
-    action = int(input("""User options:
-1. Check Balance
-2. Deposit Money
-3. Withdraw Money
-4. Exit
+username = first_name.lower() + last_name.lower()
+
+full_name = first_name.title() + " " + last_name.title()
+
+print(f"""
+Full Name: {full_name}
+Username: {username}
+Total Characters: {len(full_name)}
 """)
-)
-    if action == 1:
-        print(f"Current Balance: {balance}")
-        
-    if action == 2:
-        deposit = int(input("Enter your deposit amount: "))
-        balance += deposit
-        print(f"Current Balance after Deposit: {balance}")
-        
-    if action == 3:
-        withdrawal = int(input("Enter your withdrawal amount: "))
-        if withdrawal > balance:
-            print("Withdrawal amount is more than balance")
-        else:
-            balance -= withdrawal
-            print(f"Current Balance after withdrawal: {balance}")
-            
-    if action == 4:
-        print("No actions")
-        break
