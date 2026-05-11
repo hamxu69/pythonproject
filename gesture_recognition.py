ad1475fc31aa41061067b6fc54f79f2b73b120c8
Hamza-Sama <hamxu69@gmail.com>
2026-05-11 12:14:18 +0500

    Update gesture_recognition.py

--- gesture_recognition.py
+++ gesture_recognition.py
@@ -1,10 +1,5 @@
-numbers = [1,2,3,4,5,6,7,8,9,10]
-names = ["Hamza", "Ali", "Ahmed", "Usman"]
-squares = [num * num for num in numbers]
-odd = [num for num in numbers if num % 2 != 0]
-even = [num for num in numbers if num % 2 == 0]
-upper_case = [word.upper() for word in names]
-print(squares)
-print(odd)
-print(even)
-print(upper_case)
+i = 0
+
+while i >= 10:
+    print(i)
+    i -= 1
