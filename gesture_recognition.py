419d924c411f71c0cc37c08e227708d08e9b4831
Hamza-Sama <hamxu69@gmail.com>
2026-05-11 13:00:31 +0500

    Update gesture_recognition.py

--- gesture_recognition.py
+++ gesture_recognition.py
@@ -1,5 +1,29 @@
-i = 0
+balance = 100000
 
-while i >= 10:
-    print(i)
-    i -= 1
+while True:
+    action = int(input("""User options:
+1. Check Balance
+2. Deposit Money
+3. Withdraw Money
+4. Exit
+""")
+)
+    if action == 1:
+        print(f"Current Balance: {balance}")
+        
+    if action == 2:
+        deposit = int(input("Enter your deposit amount: "))
+        balance += deposit
+        print(f"Current Balance after Deposit: {balance}")
+        
+    if action == 3:
+        withdrawal = int(input("Enter your withdrawal amount: "))
+        if withdrawal > balance:
+            print("Withdrawal amount is more than balance")
+        else:
+            balance -= withdrawal
+            print(f"Current Balance after withdrawal: {balance}")
+            
+    if action == 4:
+        print("No actions")
+        break
