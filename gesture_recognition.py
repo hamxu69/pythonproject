b9cea557d94997a333880d61d1c34aa4b9115558
Hamza-Sama <hamxu69@gmail.com>
2026-05-12 15:03:44 +0500

    commit

--- gesture_recognition.py
+++ gesture_recognition.py
@@ -1,12 +1,38 @@
-first_name = input("Enter Your First Name: ").strip()
-last_name = input("Enter Your Last Name: ").strip()
-
-username = first_name.lower() + last_name.lower()
-
-full_name = first_name.title() + " " + last_name.title()
-
-print(f"""
-Full Name: {full_name}
-Username: {username}
-Total Characters: {len(full_name)}
-""")
+students = []
+student = {
+    "name": "",
+    "marks": "",
+    "grade": ""
+}
+while True:
+    name = input("Enter your name: ")
+    if name == 'exit':
+        for i in students:
+            print(f"name: {i["name"]}")
+        break
+    marks = int(input("Enter your marks: "))
+    grade = ''
+    if marks >= 90:
+        grade = "A"
+        student["name"] = name.title()
+        student["marks"] = marks
+        student["grade"] = grade
+        
+    elif marks >= 70:
+        grade = "B"
+        student["name"] = name.title()
+        student["marks"] = marks
+        student["grade"] = grade
+        
+    elif marks >= 50:
+        grade = "C"
+        student["name"] = name.title()
+        student["marks"] = marks
+        student["grade"] = grade
+        
+    elif marks < 50:
+        grade = "F"
+        student["name"] = name.title()
+        student["marks"] = marks
+        student["grade"] = grade
+    students.append(student)
